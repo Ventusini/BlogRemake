5278c38c4e1bb28e6ddce9b3ce5c828452ae69b8
Ventusini <freeasaventus@gmail.com>
2017-05-19 17:01:43 +0000

    Delete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,8 @@ class ArticlesController < ApplicationController
       @articles = Article.all
    end
    
+   
+   
    def create 
       @article = Article.new(article_params)
       if @article.save 
@@ -19,6 +21,13 @@ class ArticlesController < ApplicationController
    
    def show
       @article = Article.find(params[:id])
+   end
+   
+   def destroy
+      @article = Article.find(params[:id])
+      @article.destroy
+      flash[:notice] = "Article Destroyed"
+      redirect_to articles_path
    end
    
    def edit
