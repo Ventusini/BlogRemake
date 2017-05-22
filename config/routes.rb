ae9226a9038211a1d155342e536504f620b5ff62
Ventusini <freeasaventus@gmail.com>
2017-05-22 23:14:38 +0000

    uncompleted

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,11 @@ Rails.application.routes.draw do
   resources :articles
   
   get 'signup', to: 'users#new'
-  post 'users', to: 'users#create'
   resources :users, except:[:new]
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
